055b0064eaa438f07e6c33ff8dcfe45cf5da9a75
Keresoro <38075826+Keresoro@users.noreply.github.com>
2019-03-03 02:10:17 +0000

    Evrything is working for Add, Search, Update and Delete buttons

--- app/src/main/java/com/example/Week_3_Databases_SQLite/MainActivity.kt
+++ app/src/main/java/com/example/Week_3_Databases_SQLite/MainActivity.kt
@@ -12,12 +12,34 @@ class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        setSupportActionBar(toolbar1)
 
-        helper = MyHelper (this)
-
-
-
-        Add.setOnClickListener { Toast.makeText(this, "You clicked me ", Toast.LENGTH_SHORT).show() }
-        Search.setOnClickListener { Toast.makeText(this, "You clicked me!", Toast.LENGTH_SHORT).show()}
+        helper = MyHelper(ctx = this)
+        Add.setOnClickListener {
+            val title = BTitle.text.toString()
+            val artist = BArtist.text.toString()
+            val year = BYear.text.toString().toLong()
+            val id = helper.insert(title, artist, year)
+            Toast.makeText( this,"You successfully added this song", Toast.LENGTH_LONG).show()
         }
+        Search.setOnClickListener {
+            val id = BId.text.toString().toLong()
+            val database = helper.search(id)
+            BTitle.setText(database?.title)
+            BArtist.setText(database?.artist)
+            BYear.setText(database?.year.toString())
+            Toast.makeText( this,"You successfully searched for this song", Toast.LENGTH_LONG).show()
+        }
+        Uptade.setOnClickListener {
+            val id = BId.text.toString().toLong()
+            val title = BTitle.text.toString()
+            val artist = BArtist.text.toString()
+            val year = BYear.text.toString().toLong()
+            val uptade = helper.uptade(id, title, artist, year)
+        }
+        Delete.setOnClickListener {
+            val id = BId.text.toString().toLong()
+            val delete = helper.delete(id)
+        }
+    }
 }
--- app/src/main/java/com/example/Week_3_Databases_SQLite/MyHelper.kt
+++ app/src/main/java/com/example/Week_3_Databases_SQLite/MyHelper.kt
@@ -3,13 +3,15 @@ package com.example.Week_3_Databases_SQLite
 import android.database.sqlite.SQLiteOpenHelper
 import android.database.sqlite.SQLiteDatabase
 import android.content.Context
+import android.support.v7.widget.DialogTitle
+import android.widget.TableRow
 
-class MyHelper(ctx:Context) : SQLiteOpenHelper(ctx,"TestDB", null, 1) {
+data class Song (val id: Long, val title: String, val artist: String, val year: Long)
+class MyHelper(ctx:Context) : SQLiteOpenHelper(ctx,"MyDatabase", null, 1) {
 
 
     override fun onCreate(db: SQLiteDatabase) {
-
-        db.execSQL ("CREATE TABLE IF NOT EXISTS People (Id INTEGER PRIMARY KEY, Title VARCHAR(255), Artist VARCHAR(255), Year INTEGER)")
+        db.execSQL ("CREATE TABLE IF NOT EXISTS People (Id INTEGER PRIMARY KEY, Title VARCHAR(255), Artist VARCHAR(255), Year LONG)")
     }
 
     override fun onUpgrade(db:SQLiteDatabase, oldVersion:Int, newVersion:Int) {
@@ -17,21 +19,20 @@ class MyHelper(ctx:Context) : SQLiteOpenHelper(ctx,"TestDB", null, 1) {
         onCreate(db)
     }
 
-    fun insert(Title: String, Aritst: String, Year: Long) : Long? {
-        val db = getWritableDatabase()
-        val stmt = db.compileStatement ("INSERT INTO people(Title,Artist,Year) VALUES (?, ?, ?)");
-        stmt.bindString (1, Title)
-        stmt.bindString (2, Aritst )
-        stmt.bindLong (3, Year)
+    fun insert(title: String, artist: String, year: Long) : Long{
+        val db = writableDatabase
+        val stmt = db.compileStatement ("INSERT INTO People (Title, Artist, Year) VALUES (?, ?, ?)");
+        stmt.bindString (1, title)
+        stmt.bindString (2, artist)
+        stmt.bindLong (3, year)
         val id = stmt.executeInsert()
         return id
     }
-    fun search(Id: Long): Long? {
-        val db = getReadableDatabase()
-        val cursor = db.rawQuery("SELECT * FROM People WHERE Id=?", arrayOf<String>(Id.toString()) )
-
+    fun search(id: Long) : Song? {
+        val db = readableDatabase
+        val cursor = db.rawQuery("SELECT * FROM People WHERE Id=?", arrayOf(id.toString()) )
             if (cursor.moveToFirst()) {
-                val p = Song ( //TODO CREATE A SONG OBJECT! A CLASS THAT HAVE -> data class Point(val x:Int, val y: Int) <- SEE WEEK 1 DATA CLASS!!
+                val p = Song ( cursor.getLong(cursor.getColumnIndex("Id")),
                     cursor.getString(cursor.getColumnIndex("Title")),
                     cursor.getString(cursor.getColumnIndex("Artist")),
                     cursor.getLong(cursor.getColumnIndex("Year"))
@@ -41,9 +42,28 @@ class MyHelper(ctx:Context) : SQLiteOpenHelper(ctx,"TestDB", null, 1) {
             }
                 cursor.close()
                 return null
-            }
-        }
+    }
+    fun uptade(id: Long, title: String, artist: String, year: Long) : Int {
+        val db = writableDatabase
+        val stmt = db.compileStatement ("UPDATE People SET Title=?, Artist=?, Year=? WHERE Id=?");
+        stmt.bindString (1, title)
+        stmt.bindString (2, artist)
+        stmt.bindLong (3, year)
+        stmt.bindLong(4, id)
+        val nAffectedRows = stmt.executeUpdateDelete();
+        return nAffectedRows
+    }
+    fun delete(id: Long) : Int {
+        val db = writableDatabase
+        val stmt = db.compileStatement  ("DELETE FROM People WHERE Id=?");
+        stmt.bindLong(1, id)
+        val nAffectedRows = stmt.executeUpdateDelete();
+        return nAffectedRows
+    }
+
 }
+
+
 
 
 
